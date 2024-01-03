427773dc8769163ec85b0ec620336df80db8e8a3
lsy <1829644602@qq.com>
2024-01-03 19:30:06 +0800

    version 1.0

---
+++ calculator/Main.py
@@ -0,0 +1,8 @@
+from viewPlot import *
+# D:\Python\Python_3.7.2\Lib\site-packages\matplotlib\backends\_backend_tk.py
+# 613,688行做了改动
+# D:\Python\Python_3.7.2\Lib\site-packages\matplotlib\backend_bases.py
+# 2640行做了改动
+MainWin = MainWindow()
+mainloop()
+
